0394ae56e2b7900e3e104db70781e3d0ef8ab088
Jacob Zimmerman <sad2project@gmail.com>
2014-07-02 00:08:52 -0500

    Lots of work on RandomElementChooser derivatives
    Separated parts out
    Tweaks to Roll

--- EZGames/src/ezgames/udicesys/diceModels/URoll.java
+++ EZGames/src/ezgames/udicesys/diceModels/URoll.java
@@ -12,25 +12,21 @@ import ezgames.utils.collections.simple.SimpleCollection;
 import ezgames.utils.exceptions.NullArgumentException;
 
 @Immutable
-public class URoll implements Roll
-{	
-	URoll(Die die, Face face) throws NullArgumentException
-	{
-		//TODO: add messages
-		DataChecker.checkDataNotNull(die, "");
-		DataChecker.checkDataNotNull(face, "");
-		this.die = die;
-		this.face = face;
-	}
-	
+public final class URoll implements Roll
+{
+	//***************************************************************************
+	// Public API methods
+	//***************************************************************************
 	public Die die()
 	{
 		return die;
 	}
+	
 	public Face rolledFace()
 	{
 		return face;
 	}
+	
 	public SimpleCollection<FaceValue> rolledFaceValues()
 	{
 		try
@@ -38,20 +34,36 @@ public class URoll implements Roll
 			return SimpleCollection.from(face);
 		}
 		catch (NullArgumentException e)
-		{ 
+		{
 			return null;
 		}
 	}
+	
 	public SimpleCollection<Relationship> rolledRelationships()
 	{
 		return face.listRelationships();
 	}
+	
 	public SimpleCollection<Effect> rolledEffects()
 	{
-		//TODO:return face.listEffects() 
+		// TODO:return face.listEffects()
 		return null;
 	}
 	
-	private Die die;
-	private Face face;
+	//***************************************************************************
+	// Package-private constructor
+	//***************************************************************************
+	URoll(Die die, Face face) throws NullArgumentException
+	{
+		DataChecker.checkDataNotNull(die, "Cannot create a Roll with a null Die");
+		DataChecker.checkDataNotNull(face, "Cannot create a Roll with a null Face");
+		this.die = die;
+		this.face = face;
+	}
+	
+	//***************************************************************************
+	// Private fields
+	//***************************************************************************
+	private final Die die;
+	private final Face face;
 }
--- EZGames/src/ezgames/utils/collections/package-info.java
+++ EZGames/src/ezgames/utils/collections/package-info.java
@@ -5,9 +5,9 @@
  *and {@link ezgames.utils.collections.MlList MlList}</li>
  *	<li>The {@link ezgames.utils.collections.SimpleCollection SimpleCollection} interface</li>
  *	<li>The {@link ezgames.utils.collections.Stackable Stackable} interface</li>
- *	<li>The {@link ezgames.utils.collections.RandomElementChooser RandomElementChooser} class</li>
+ *	<li>The {@link ezgames.utils.collections.rec.RandomElementChooser RandomElementChooser} class</li>
  *</ol>
- *<p> It all started with <code>ZArray</code>.  I wanted to use a small, fast, 
+ *<p> It all started with {@code ZArray}.  I wanted to use a small, fast, 
  *and immutable collection within my model objects.</p>
  *<p>It had to be small because, with the exception of dice's collection of faces,
  *A great majority of the time, collections within the system's domain objects
@@ -22,40 +22,40 @@
  *want anything to accidentally change it.  I also liked the idea of keeping the
  *the collection class extra simple by not allowing the addition or subtraction
  *of elements once it's created.</p>
- *<p>Because of all of this, the <code>ZArray</code> didn't fit well with Java's
- *<code>Collection</code> interface. So, for a while, I was just passing around 
- *<code>Iterable</code>s.  But that led to having a utility class that would do
- *the <code>contains(), get(), indexOf(),</code> and <code>size()</code> methods
- *from the <code>Iterable</code>s.  These were highly inefficient, iterating over
+ *<p>Because of all of this, the {@code ZArray} didn't fit well with Java's
+ *{@code Collection} interface. So, for a while, I was just passing around 
+ *{@code Iterable}s.  But that led to having a utility class that would do
+ *the {@code contains(), get(), indexOf(),} and {@code size()} methods
+ *from the {@code Iterable}s.  These were highly inefficient, iterating over
  *the collection for everything.</p>
- *<p>Thus the <code>SimpleCollection</code> interface was born.  It provided the
+ *<p>Thus the {@code SimpleCollection} interface was born.  It provided the
  *previously mentioned methods, plus methods to supply the collections' 
- *<code>Iterator</code>s and Java 8 <code>Stream</code>s.  Lastly, it has a
- *static method that transforms any type of Java collection or <code>Iterable</code>
- *into a <code>SimpleCollection</code>.</p>
+ *{@code Iterator}s and Java 8 {@code Stream}s.  Lastly, it has a
+ *static method that transforms any type of Java collection or {@code Iterable}
+ *into a {@code SimpleCollection}.</p>
  *<p>With this, I was able to provide one unified collection interface throughout
  *the UDice System without having to deal with the lowest common denominator
- *of just <code>Iterable</code>.  It also makes the collections effectively
+ *of just {@code Iterable}.  It also makes the collections effectively
  *immutable, except in the case of the elements being mutated from outside the
  *collection.</p>
- *<p>I also created another collection, called <code>MlList</code> to allow me
+ *<p>I also created another collection, called {@code MlList} to allow me
  *to build collections easily and immutably.  It's named MlList because I "stole"
  *the idea from a language called ML, a functional programming language.  It
- *implements the <code>SimpleCollection</code> interface, too, so it can be used
- *within the system.  <code>MlList</code>, alongside the <code>Stackable</code>
+ *implements the {@code SimpleCollection} interface, too, so it can be used
+ *within the system.  {@code MlList}, alongside the {@code Stackable}
  *interface, allows for quick, easy, and fluent creation of collections.  See
  *{@link ezgames.utils.collections.Stackable Stackable} for more information on
  *that.</p>
- *<p>Lastly, we have <code>RandomElementChooser</code>.  This class was created
- *to decouple the choosing of random faces on a die from the <code>Die</code>
- *classes.  It works with any <code>SimpleCollection</code> to randomly choose
+ *<p>Lastly, we have {@code RandomElementChooser}.  This class was created
+ *to decouple the choosing of random faces on a die from the {@code Die}
+ *classes.  It works with any {@code SimpleCollection} to randomly choose
  *an element from it. It uses dependency injection so you can provide your own
  *random number generator (implementing the {@link ezgames.math.random.SimpleRandom 
  *SimpleRandom} interface) as well as any rules you want the chosen elements to
- *follow. It even supports weighted randomization through the use of weighted 
- *collections (see {@link ezgames.util.Weighted}). Check out 
- *{@link ezgames.utils.collections.RandomElementChooser RandomElementChooser} 
- *for more details.</p>
+ *follow, using the Strategy Pattern. It even supports weighted randomization 
+ *through the use of weighted collections (see {@link ezgames.util.Weighted}). 
+ *Check out {@link ezgames.utils.collections.rec.RandomElementChooser 
+ *RandomElementChooser} for more details.</p>
  */
 package ezgames.utils.collections;
 
