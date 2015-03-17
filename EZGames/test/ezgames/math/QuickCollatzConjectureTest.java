b1ab59ce9860dc9c088b17b376d804b21711da9a
Jacob Zimmerman <jzimmerman@collaborative.com>
2015-03-17 16:10:53 -0500

    Removed CollatzConjecture
    moved to playingWithJava project

--- EZGames/test/ezgames/math/QuickCollatzConjectureTest.java
+++
@@ -1,9 +0,0 @@
-package ezgames.math;
-
-public class QuickCollatzConjectureTest
-{
-	public static void main(String[] args)
-	{
-		CollatzConjecture.stream(365).forEach(i -> System.out.println(i));		
-	}
-}
